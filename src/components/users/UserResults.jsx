583767d357d06acdf637ab4eee2fe699f1606a6e
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-01-23 16:11:01 +0100

    feat: clean up fetch users

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,14 +1,10 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import Spinner from "../layout/Spinner";
 import UserItem from "./UserItem";
 import GithubContext from "../../context/github/GithubContext";
 
 function UserResults() {
-  const { users, loading, fetchUsers } = useContext(GithubContext);
-
-  useEffect(() => {
-    fetchUsers();
-  }, [fetchUsers]);
+  const { users, loading } = useContext(GithubContext);
 
   if (!loading) {
     return (
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,12 +9,15 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
-    loading: true,
+    loading: false,
   };
 
   const [state, dispatch] = useReducer(GithubReducer, initialState);
 
+  // Get initial users (testing purposes)
   const fetchUsers = async () => {
+    setLoading();
+
     const response = await fetch(`${GITHUB_URL}/users`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
@@ -28,6 +31,9 @@ export const GithubProvider = ({ children }) => {
       payload: data,
     });
   };
+
+  // Set loading to true
+  const setLoading = () => dispatch({ type: "SET_LOADING" });
 
   return (
     <GithubContext.Provider
